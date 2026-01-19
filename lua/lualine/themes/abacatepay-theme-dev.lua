3dc18a8dcb1264670f9519ecb276d3418d2908e3
Victor Albuquerque <albq.victor@gmail.com>
2026-01-19 18:52:18 -0300

    wip: i'll try make this again later

--- lua/lualine/themes/abacatepay-theme-dev.lua
+++ lua/lualine/themes/abacatepay-theme-dev.lua
@@ -1,38 +1,38 @@
 local M = {}
 
 M.normal = {
-	a = { fg = "#101213", bg = "#FEA94C", bold = true },
-	b = { fg = "#F5E6D3", bg = "#161819" },
-	c = { fg = "#F5E6D3", bg = "#101213" },
+	a = { fg = "#101213", bg = "#FFC266", bold = true },
+	b = { fg = "#FAF0D3", bg = "#161819" },
+	c = { fg = "#FAF0D3", bg = "#101213" },
 }
 
 M.insert = {
-	a = { fg = "#101213", bg = "#8BC48B", bold = true },
-	b = { fg = "#F5E6D3", bg = "#161819" },
-	c = { fg = "#F5E6D3", bg = "#101213" },
+	a = { fg = "#101213", bg = "#FAF0D3", bold = true },
+	b = { fg = "#FAF0D3", bg = "#161819" },
+	c = { fg = "#FAF0D3", bg = "#101213" },
 }
 
 M.command = {
-	a = { fg = "#101213", bg = "#F5E6D3", bold = true },
-	b = { fg = "#F5E6D3", bg = "#161819" },
-	c = { fg = "#F5E6D3", bg = "#101213" },
+	a = { fg = "#101213", bg = "#FAF0D3", bold = true },
+	b = { fg = "#FAF0D3", bg = "#161819" },
+	c = { fg = "#FAF0D3", bg = "#101213" },
 }
 
 M.visual = {
 	a = { fg = "#101213", bg = "#D4A373", bold = true },
-	b = { fg = "#F5E6D3", bg = "#161819" },
-	c = { fg = "#F5E6D3", bg = "#101213" },
+	b = { fg = "#FAF0D3", bg = "#161819" },
+	c = { fg = "#FAF0D3", bg = "#101213" },
 }
 
 M.replace = {
-	a = { fg = "#101213", bg = "#E09540", bold = true },
-	b = { fg = "#F5E6D3", bg = "#161819" },
-	c = { fg = "#F5E6D3", bg = "#101213" },
+	a = { fg = "#101213", bg = "#D09540", bold = true },
+	b = { fg = "#FAF0D3", bg = "#161819" },
+	c = { fg = "#FAF0D3", bg = "#101213" },
 }
 
 M.inactive = {
-	a = { fg = "#F5E6D3", bg = "#161819" },
-	b = { fg = "#F5E6D3", bg = "#161819" },
+	a = { fg = "#FAF0D3", bg = "#161819" },
+	b = { fg = "#FAF0D3", bg = "#161819" },
 	c = { fg = "#9B8B7A", bg = "#101213" },
 }
 
